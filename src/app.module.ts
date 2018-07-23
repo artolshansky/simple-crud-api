2f248b59121ecd4b4b04e555ce700c98bd5c4ebb
olshansky <olshansky0@gmail.com>
2018-07-23 19:47:00 +0300

    fix(core): fix db options

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PageModule } from './page/page.module';
+import { Page } from './page/page.entity';
+
+const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PageModule]
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    url: `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+    entities: [Page],
+    synchronize: true
+  }), PageModule]
 })
 export class AppModule {}
