bdb535b87c049d973ef5c76507c882ce0fc41f03
olshansky <olshansky0@gmail.com>
2018-07-23 19:47:00 +0300

    fix(core): fix db options

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PageModule } from './page/page.module';
+import { Page } from './page/page.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PageModule]
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    url: 'mongodb://localhost:27017',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'simple-crud-api',
+    entities: [Page],
+    synchronize: true
+  }), PageModule]
 })
 export class AppModule {}
